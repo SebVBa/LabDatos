1e67f784f537a70d2bda492469fb856a8017d977
Sebastian Villega Barquero <sebastianbarquerocr@gmail.com>
2026-06-16 09:51:56 -0600

    Version 2
    Se implementa variables para que se lean dentro de app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCRIPTION=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
 
 const app = express();
 
 app.get("/", (req, res) =>{
-res.send ("<h1>Hola</h1>");
+res.send (`<h1>${NAME}</h1> <p>Version ${VERSION}</p> <p>Descripcion ${DESCRIPTION}</p> <p>En el puerto ${PORT}</p>`);
 });
 
 app.listen(4000, () =>{
- console.log("servidor ejecutandose")
+ console.log(`${NAME} ejecutandose en el puerto http://localhost:${PORT}`)
 });
